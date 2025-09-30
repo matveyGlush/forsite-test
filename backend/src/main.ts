2bca51deeb5f7d47521402043871a6d516bb6ec8
Matvey Glushko <matveyglushkospb@gmail.com>
2025-09-30 20:02:28 +0300

    problem with reload front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: '*',
-    exposedHeaders: '*',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: '*',
+  //   exposedHeaders: '*',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
