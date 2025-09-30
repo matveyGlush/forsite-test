db3262ff67850e6c58771a7e6f61f606c0cd1a92
Matvey Glushko <matveyglushkospb@gmail.com>
2025-09-30 19:15:17 +0300

    remove html

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
+import type { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response) {
+    return res.sendFile(join(__dirname, 'data.json'));
   }
 }
